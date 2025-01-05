0ea9522b88ea5601a31207e187f4a4b82ba298d3
mcuong <huynhmc2804@gmail.com>
2025-01-05 23:38:04 +0700

    05/01 kết nối mongodb

--- movie-watching-app-back-end/index.js
+++ movie-watching-app-back-end/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+const database = require('./config/database');
+database.connect();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');

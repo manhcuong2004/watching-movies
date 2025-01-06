e545e9079fe19e96d7404219be0d17426370f18f
mcuong <huynhmc2804@gmail.com>
2025-01-06 17:22:37 +0700

    06/01

--- movie-watching-app-back-end/index.js
+++ movie-watching-app-back-end/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
+
 const database = require('./config/database');
 database.connect();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const route = require("./routes/client/index-route");
+route(app);
+
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);

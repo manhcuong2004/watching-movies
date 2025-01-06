fe60b2ef87d43de41c2e2b78e8ee49551d9f7062
akhoa6204 <khoaanh662004@gmail.com>
2025-01-06 16:08:46 +0700

    updateupdate

--- movie-watching-app-front-end/src/pages/LoginRegister/index.js
+++ movie-watching-app-front-end/src/pages/LoginRegister/index.js
@@ -1,8 +1,13 @@
 import styles from './styles.module.css'
 import clsx from 'clsx'
 import Nav from '../../components/components/Nav'
-
+import {useState} from 'react'
 function LoginRegister(){
+    const [islogin, setIsLogin] = useState(true)
+    const handleClick= (e)=>{
+        e.preventDefault()
+        setIsLogin(!islogin)
+    }
     return(
         <div className ={styles.loginRegister_container}>
             <div className={styles.nav_box}>
@@ -10,8 +15,8 @@ function LoginRegister(){
             </div>
             <div className={styles.form_box}>
                 <div className={styles.heading}>
-                    <a href ='' className={styles.active}>Login</a>
-                    <a href=''>Register</a>
+                    <a href ='' className={islogin ? styles.active : ''} onClick={handleClick}>Login</a>
+                    <a href='' className={!islogin ? styles.active : ''} onClick={handleClick}>Register</a>
                 </div>
                 <div className={styles.form}>
                     <form action="">
@@ -21,16 +26,19 @@ function LoginRegister(){
                         <div className={clsx(styles.form_group, styles.full)}>
                             <input type="password" name="" id="password" placeholder="Password"/>
                         </div>
+                        <div className={clsx(styles.form_group, styles.full, !islogin ? styles.active : '')}>
+                            <input type="email" name="" id="email" placeholder="Email"/>
+                        </div>
                         <div className={styles.form_group}>
                             <input type="checkbox" name="" id="remember"/>
                             <label for="remember">Remember me</label>
                         </div>
                         <div className={styles.form_group}>
-                            <a href="">Forgot Password</a>
+                            <a href="" onClick={handleClick}>Forgot Password</a>
                         </div>
                         <div className={styles.form_group}>
-                            <input type="submit" value="Login"/>
-                            <input type="submit" value="Register"/>
+                            <input type="submit" value="Login"className={islogin ? styles.active : ''}/>
+                            <input type="submit" value="Register" className={!islogin ? styles.active : ''}/>
                         </div>
                     </form>
                 </div>
